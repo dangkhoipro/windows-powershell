3e53e49fa6419dbb2135868981b2f01ff1600ed3
khoind4 <khoind4@vng.com.vn>
2024-06-05 10:58:15 +0700

    feat: add init function (split pane, run custom command)

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -9,49 +9,67 @@ if (Test-Path($ChocolateyProfile)) {
   Import-Module "$ChocolateyProfile"
 }
 
-function Prompt {
-  $loc = $executionContext.SessionState.Path.CurrentLocation;
+# function Prompt {
+#   $loc = $executionContext.SessionState.Path.CurrentLocation;
+#
+#   $out = ""
+#     if ($loc.Provider.Name -eq "FileSystem") {
+#       $out += "$([char]27)]9;9;`"$($loc.ProviderPath)`"$([char]27)\"
+#     }
+#   $out += "PS $loc$('>' * ($nestedPromptLevel + 1)) ";
+#   return $out
+# }
 
-  $out = ""
-    if ($loc.Provider.Name -eq "FileSystem") {
-      $out += "$([char]27)]9;9;`"$($loc.ProviderPath)`"$([char]27)\"
+function parseAlias {
+  param(
+      [string]$AliasPath
+      )
+
+    $aliases = @{
+      "zsp" = "D:\z-social-plus-miniapp\"
+        "zsp2" = "D:\z-social-plus-miniapp-copy\"
+        "mb" = "d:/zalo-ads-mobile"
+        "mb2" = "d:/zalo-ads-mobile-copy"
+        "mp" = "d:/instant-page"
+        "mp2" = "d:/instant-page-copy"
+        "arc" = "d:/ads-review"
+        "mock" = "D:\mock-server-zsp\"
+        "nvim" = "~/appData/Local/my-nvim"
+        "lazy" = "~/appData/Local/lazyvim/"
+        "note" = "~/notes"
+        "notew" = "~/notes/work"
+        "notep" = "~/notes/personal/"
+        "lazygit" = "~/appData/Roaming/lazygit/"
+        "port" = "D:/personal-projects/portfolio"
+        "admin" = "D:/zalo-ads-admin"
+        "adv2" = "D:/zalo-ads-v2"
+        "mad" = "D:/new-new-mock-ads"
+        "zad" = "D:/z-ad"
+        "zad2" = "D:/z-ad-copy"
+# Add more aliases as needed
     }
-  $out += "PS $loc$('>' * ($nestedPromptLevel + 1)) ";
-  return $out
+
+  if ($aliases.ContainsKey($AliasPath)) {
+    $Path = $aliases[$AliasPath]
+    $Path2 = $aliases[$AliasPath + "2"]
+
+    return @($Path, $Path2)
+  } else {
+    return $null
+  }
 }
 
 function go {
   param(
-    [string]$AliasPath,
-    [string]$Command
-  )
+      [string]$AliasPath,
+      [string]$Command
+      )
 
-  # Define aliases and their corresponding paths
-  $aliases = @{
-    "zsp" = "D:\z-social-plus-miniapp\"
-    "zsp1" = "D:\z-social-plus-miniapp-copy\"
-    "mb" = "d:/zalo-ads-mobile"
-    "mb1" = "d:/zalo-ads-mobile-copy"
-    "mp" = "d:/instant-page"
-    "mp1" = "d:/instant-page-copy"
-    "arc" = "d:/ads-review"
-    "mock" = "D:\mock-server-zsp\"
-    "nvim" = "~/appData/Local/my-nvim"
-    "lazy" = "~/appData/Local/lazyvim/"
-    "note" = "~/notes"
-    "notew" = "~/notes/work"
-    "notep" = "~/notes/personal/"
-    "lazygit" = "~/appData/Roaming/lazygit/"
-    "port" = "D:/personal-projects/portfolio"
-    "admin" = "D:/zalo-ads-admin"
-    "adv2" = "D:/zalo-ads-v2"
-    # Add more aliases as needed
-  }
+  $Paths = parseAlias($AliasPath)
 
-  # Check if the alias exists in the aliases dictionary
-  if ($aliases.ContainsKey($AliasPath)) {
-    $Path = $aliases[$AliasPath]
-      Set-Location -Path $Path
+# Check if the alias exists in the aliases dictionary
+  if ($Paths) {
+    Set-Location -Path $Paths[0]
 
       if ($Command) {
         Invoke-Expression -Command $Command
@@ -61,3 +79,23 @@ function go {
   }
 }
 
+function init {
+  param(
+      [string]$AliasPath
+      )
+
+  $paths = parseAlias($AliasPath)
+
+  if (!$paths) {
+    Write-Host "Alias not found."
+    return
+  }
+
+# open a new pane & set directory
+  wt.exe -w 0 -d $paths[0] nvim `; sp -V -d $paths[0] lazygit `; sp -V -d $paths[1] `; mf -direction 8
+
+  # move to the middle pane (lazygit)
+  # direction left:1, right:2, first:8
+  # wt -w 0 move-focus --direction 1
+  # Invoke-Expression -Command "lazygit"
+}
